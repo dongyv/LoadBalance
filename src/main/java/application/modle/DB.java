8b3452d8a1f383da0d610b62f3eee4c1bbc25479
dongyv <529979142@qq.com>
2018-11-06 15:12:58 +0800

    sql模版

---
+++ src/main/java/application/modle/DB.java
@@ -0,0 +1,32 @@
+package application.modle;
+
+import java.sql.*;
+
+public class DB {
+    private static Connection con;
+
+    public static void init(String driver,String config) throws ClassNotFoundException, SQLException {
+        Class.forName(driver);
+        con = DriverManager.getConnection(config);
+    }
+
+    public static <T> void store(T entry) throws Exception{
+        PreparedStatement s = buildTStatement(entry);
+        executeStatement(s);
+    }
+
+    private static <T> PreparedStatement buildTStatement(T entry) throws SQLException {
+        String sql = "";
+        PreparedStatement s = con.prepareStatement(sql);
+        return s;
+    }
+
+    private static void executeStatement(PreparedStatement s) throws SQLException {
+        s.execute();
+        s.close();
+    }
+
+    public static void close() throws SQLException {
+        con.close();
+    }
+}
